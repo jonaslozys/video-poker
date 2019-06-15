18216eb578d750f41fc53146a8aaab15e5d0049d
Jonas <jonas.lozys@protonmail.com>
2019-06-15 22:44:54 +0300

    Add user input parser.

--- VideoPoker/Program.cs
+++ VideoPoker/Program.cs
@@ -8,11 +8,41 @@ namespace VideoPoker
 {
     class Program
     {
+        public static List<int> ParseInput(string inputString)
+        {
+            List<int> selectedCards = new List<int>();
+
+            foreach(char character in inputString)
+            {
+                int cardNumber = (int)Char.GetNumericValue(character);
+
+                if (cardNumber > 0 && cardNumber < 6 && !selectedCards.Contains(cardNumber))
+                {
+                    selectedCards.Add(cardNumber);
+                }
+            }
+
+            return selectedCards;
+        }
         static void Main(string[] args)
         {
             Dealer dealer = new Dealer();
-            dealer.Deal();
-            Console.ReadKey();
+
+            while (true)
+            {
+                dealer.Deal();
+
+                Console.WriteLine("Enter number(s) of cards to hold. (1 - 5)");
+                string selectedCards = Console.ReadLine();
+                int[] cardNumbers = ParseInput(selectedCards).ToArray();
+                foreach(int number in cardNumbers)
+                {
+                    Console.WriteLine($"Holding {number}");
+
+                }
+
+            }
+
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
