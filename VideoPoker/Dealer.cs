e75b5e2dcdf8420a0e82f401f22711a1e8b95437
Jonas <jonas.lozys@protonmail.com>
2019-06-15 20:55:42 +0300

    Add Dealer class.

---
+++ VideoPoker/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPoker
+{
+    class Dealer
+    {
+        public Card[] playerHand;
+        public CardDeck cardDeck;
+
+        public Dealer()
+        {
+            playerHand = new Card[5];
+            cardDeck = new CardDeck();
+        }
+
+
+        public void GetHand()
+        {
+            for (int index = 0; index < 5; index++)
+            {
+                playerHand[index] = cardDeck.getDeck[index];
+            }
+        }
+    }
+}
