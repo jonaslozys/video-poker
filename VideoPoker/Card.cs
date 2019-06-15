7905c144c7cd2918784c38504f7fa9f620c27bb4
Jonas <jonas.lozys@protonmail.com>
2019-06-15 20:37:29 +0300

    Add Card class.

---
+++ VideoPoker/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPoker
+{
+    class Card
+    {
+        public enum Suit
+        {
+            HERTS,
+            SPADES,
+            DIAMONDS,
+            CLUBS
+        }
+
+        public enum Value
+        {
+            TWO = 2,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING,
+            ACE
+        }
+
+        public Suit CardSuit { get; set; }
+        public Value CardValue { get; set; }
+    }
+}
